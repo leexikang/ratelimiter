4d60c77b1560ab68b9ff76eaa137f413a6d36920
min <minsan.work@gmail.com>
2022-05-25 08:02:44 +0800

    wip

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package ratelimiter
 
-import deque "github.com/leexikang/generic-deque"
+import (
+
+	deque "github.com/leexikang/generic-deque"
+)
 
 type RequestTimeStamps struct {
 	timeStamps      deque.Deque[int64]
@@ -27,4 +30,28 @@ func (requestTimeStamps *RequestTimeStamps) EvictBefore(currentTime int64) {
 		requestTimeStamps.timeStamps.Front() < currentTime {
 		requestTimeStamps.timeStamps.PopFront()
 	}
+}
+
+
+// Create a map with UserId key and the value is requestTimeStamps
+//  If user is not created create a new one for it
+// Provide append method
+//  Append the timeStamp to the requestTimeStamps 
+//  Evict the logs with timestamp less than now - WindowTimeInSec 
+//  compare the logs size with the RequestTimeStamps' Limit, 
+//  if greateer allow, if less than denied
+
+type RateLimiter struct {
+  requestTimeStamps map[string]RequestTimeStamps 
+}
+
+type request interface {
+  Id() string 
+}
+
+func (ratelimiter *RateLimiter) insert(request request) {
+  requestTimeStamps, ok := ratelimiter.requestTimeStamps[request.Id()] 
+  if !ok {
+    panic("user has not been initialized yet")
+  }
 }
